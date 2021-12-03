d8d68ba7645268f1248f6082b82e3860d1357bfb
pimbp <70762327+pimbp@users.noreply.github.com>
2021-12-03 21:15:52 +0100

    zadanie 26.11

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(LoginEntry.Text) == false && String.IsNullOrEmpty(PassEntry.Text) == false)
+            {
+                DisplayAlert("Alert", "Login: " + LoginEntry.Text, "OK");
+            }
+            else
+            {
+                DisplayAlert("Error", "Please input both the login and the password", "OK");
+            }
+        }
+    }
+}
